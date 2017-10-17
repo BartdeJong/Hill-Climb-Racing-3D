27f7b724284eaafb8927e40f289b3029fde39913
BartdeJong <bart.dejong375@gmail.com>
2017-10-17 12:35:39 +0200

    NOOOOOOOOO ERROR AND WARNINGS ANYMOREEEEEEEEEEE!!!!!!!!!!!!

--- HillClimbRacing3DV2/build/cannon.js
+++ HillClimbRacing3DV2/build/cannon.js
@@ -7955,7 +7955,7 @@
             if(n.dot(vertex) < 0){
                 //console.error(".faceNormals[" + i + "] = Vec3("+n.toString()+") looks like it points into the shape? The vertices follow. Make sure they are ordered CCW around the normal, using the right hand rule.");
                 for(var j=0; j<this.faces[i].length; j++){
-                    console.warn(".vertices["+this.faces[i][j]+"] = Vec3("+this.vertices[this.faces[i][j]].toString()+")");
+                    //console.warn(".vertices["+this.faces[i][j]+"] = Vec3("+this.vertices[this.faces[i][j]].toString()+")");
                 }
             }
         }
